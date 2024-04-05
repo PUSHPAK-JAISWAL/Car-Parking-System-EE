3afe3c6a27f322b64ebaa4818a708b028d04b8b5
PUSHPAK-JAISWAL <149317014+PUSHPAK-JAISWAL@users.noreply.github.com>
2024-04-05 23:14:11 +0530

    second change according to the commit

--- Arduino.ino
+++ Arduino.ino
@@ -9,9 +9,9 @@ int irSensors[6] = {5, 6, 7, 8, 9, 10}; // IR sensor pins for parking slots
 int irGateEntry = 11; // IR sensor pin for gate entry
 int irGateExit = 12; // IR sensor pin for gate exit
 bool slots[6]; // parking slots status
-int slot = 6;  
+int slot = 6;
 
-void setup(){
+void setup() {
   Serial.begin(9600);
 
   for (int i = 0; i < 6; i++) {
@@ -24,26 +24,24 @@ void setup(){
   myservo.attach(3);
   myservo.write(90);
 
-  lcd.begin(20, 4);  
-  lcd.setCursor (0,1);
-  lcd.print("    Car  parking  ");
-  lcd.setCursor (0,2);
-  lcd.print("       System     ");
-  delay (2000);
-  lcd.clear();   
+  lcd.begin(20, 4);
+  lcd.setCursor(0, 0); // Set cursor to row 0
+  lcd.print("Available Slots:");
 
   Read_Sensor();
 
   int total = slots[0] + slots[1] + slots[2] + slots[3] + slots[4] + slots[5];
-  slot = slot - total; 
+  slot = slot - total;
 }
 
-void loop(){
+void loop() {
   Read_Sensor();
-  lcd.setCursor (0,0);
-  lcd.print("   Have Slot: "); 
-  lcd.print(slot);
-  lcd.print("    ");  
+  lcd.setCursor(0, 1); // Set cursor to row 1
+  for (int i = 0; i < 6; i++) {
+    lcd.print("Slot");
+    lcd.print(i + 1);
+    lcd.print(slots[i] ? ": Filled " : ": Empty ");
+  }
 
   // Send parking slots status to NodeMCU
   for (int i = 0; i < 6; i++) {
@@ -68,8 +66,8 @@ void loop(){
   }
 }
 
-void Read_Sensor(){
+void Read_Sensor() {
   for (int i = 0; i < 6; i++) {
     slots[i] = digitalRead(irSensors[i]) == LOW;
-  } 
+  }
 }
